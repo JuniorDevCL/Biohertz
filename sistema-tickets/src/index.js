ee55f337e7f7bd17c67452ad3c1c9cf6b1de1aac
JuniorDevCL <alexis.cruces@mail.udp.cl>
2025-12-28 07:12:18 +0100

    fix: Add proxy support for Google Auth

--- sistema-tickets/src/index.js
+++ sistema-tickets/src/index.js
@@ -19,6 +19,8 @@ import pool from './db.js';
 
 const app = express();
 
+app.enable('trust proxy');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), 'public')));
