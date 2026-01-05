6efc3e18e9e6e26666abafc05be9dfa4b34ee75e
JuniorDevCL <alexis.cruces@mail.udp.cl>
2026-01-05 07:33:00 +0100

    Fix: Add Tailwind build script

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ejs}",
+    "./public/**/*.{html,js,ejs}",
+    "./*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
